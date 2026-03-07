21a768d96ea30696251c086994b42e8adae78ab0
turletti <turletti@users.noreply.github.com>
2026-03-07 15:25:14 +0100

    fix free5gc upf chart

--- roles/5g/free5gc/config/files/patch_upf_wrapper.py
+++ roles/5g/free5gc/config/files/patch_upf_wrapper.py
@@ -9,7 +9,7 @@ files = [
 
 old = '    iptables -t nat -A POSTROUTING -s {{ $.Values.global.uesubnet }} -o n6 -j MASQUERADE  # route traffic comming from the UE SUBNET to the interface N6\n    mkdir -p /etc/iproute2 && echo "1200 n6if" >> /etc/iproute2/rt_tables # create a routing table for the interface N6\n    ip rule add from {{ $.Values.global.uesubnet }} table n6if   # use the created ip table to route the traffic comming from the UE SUBNET\n    ip route add default via {{ $.Values.global.n6network.gatewayIP }} dev n6 table n6if  # add a default route in the created table so that all UEs will use this gateway for external communications (target IP not in the Data Network attached to the interface N6) and then the Data Network will manage to route the traffic'
 
-new = '    iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE\n    ip route del default via {{ $.Values.global.n6network.gatewayIP }} dev n6 2>/dev/null || true'
+new = '    iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE\n    ip route del default via {{ $.Values.global.n6network.gatewayIP }} dev n6 2>/dev/null || true\n    ip route | grep "^default" | grep -v "eth0" | while read -r gw via ip dev iface rest; do ip route del default via $ip dev $iface 2>/dev/null || true; done'
 
 for f in files:
     if not os.path.exists(f):
